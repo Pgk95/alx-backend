458f29dc58d843a93b59257f5b704324ff5b7ef6
pgk95 <kalupgk@gmail.com>
2023-10-23 18:51:24 +0300

    task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,4 +5,5 @@
 def index_range(page: int, page_size: int) -> tuple:
     """return a tuple of size two containing a
     start index and an end index"""
+
     return ((page - 1) * page_size, page * page_size)
