0da7d5a007f7025b9967e57404a2b916fd45b2c3
pgk95 <kalupgk@gmail.com>
2023-10-23 13:58:35 +0300

    quick fix

--- 0-simple_helper_function.py
+++ 0-simple_helper_function.py
@@ -3,6 +3,6 @@
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """return a tuple of size two containing a 
+    """return a tuple of size two containing a
     start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
