391b9fa2c25de36badece614ba6ac3a294f75e9c
pgk95 <kalupgk@gmail.com>
2023-10-27 17:01:17 +0300

    quickf ix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,9 +10,8 @@ class BasicCache(BaseCaching):
     """
     def put(self, key, item):
         """assigning the dictionary"""
-        self.cache_data[key] = item
-        if key is None or item is None:
-            return None
+        if key is not None or item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
         """returns the value in self.cache_data[key]"""
