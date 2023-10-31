a58463cef1340e1df86ce268502af1bc03fbd832
pgk95 <kalupgk@gmail.com>
2023-10-31 22:25:59 +0300

    quick fix

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,7 +8,16 @@ app = Flask(__name__)
 babel = Babel(app)
 
 class Config(object):
-    app.config['BABEL_DEFAULT_LOCALE'] = "en"
+    app.config['BABEL_DEFAULT_LOCALE'] = "en", "fr"
     app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
 
+
+@app.route("/", strict_slashes=False)
+def index():
+    return render_template("1-index.html")
+
+
 babel.init_app(app)
+
+if __name__ == "__main__":
+    app.run()
