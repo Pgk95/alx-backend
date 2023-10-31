c50b702ed427517bb293c1565e1c886b1ee1e906
pgk95 <kalupgk@gmail.com>
2023-10-31 16:03:30 +0300

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """intialize babel flask app"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-
 babel = Babel(app)
 
-class Config(object):
-    """Config class for babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIME_ZONE = "UTC"
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """index page"""
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
