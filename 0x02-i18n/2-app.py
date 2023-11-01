d7a5d71402862d11366a4a10c34caa1a1a2452bf
pgk95 <kalupgk@gmail.com>
2023-11-01 15:32:45 +0300

    quick fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -8,7 +8,7 @@ babel = Babel(app)
 
 
 class Config(object):
-    """ App configuration """
+    """Config class for babel"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -19,13 +19,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """ get locale """
+    """get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/", methods=['GET'])
 def index():
-    """ Index route """
+    """index page"""
     return render_template("2-index.html")
 
 
