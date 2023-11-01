85b1c3d84852d3d3b8df706195e58fd0f9ee93a2
pgk95 <kalupgk@gmail.com>
2023-11-01 11:49:42 +0300

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pytohn3
+"""initialize babel request"""
+
+from flask import Flask, g, request, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """config class for babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template("2-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
