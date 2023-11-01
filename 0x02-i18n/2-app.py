38a250f6c94df24570962bba1bc3f27b7efee926
pgk95 <kalupgk@gmail.com>
2023-11-01 11:56:35 +0300

    quick fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,11 +20,12 @@ app.config.from_object(Config)
 
 @app.route("/", methods=['GET'])
 def index():
+    """index page"""
     return render_template("2-index.html")
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
