4199bb6a61e5f668a252fc5b3f34a1b427529bb9
pgk95 <kalupgk@gmail.com>
2023-11-01 15:30:52 +0300

    quick fix

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,34 +1,33 @@
-#!/usr/bin/env pytohn3
-"""initialize babel request"""
-
-from flask import Flask, g, request, render_template
+#!/usr/bin/env python3
+""" configuring route Babel """
+from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
-    """config class for babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+class Config(object):
+    """ App configuration """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """get locale"""
+def get_locale():
+    """ get locale """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/")
-def index() -> str:
-    """index page"""
+@app.route("/", methods=['GET'])
+def index():
+    """ Index route """
     return render_template("2-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
