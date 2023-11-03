79485c322237aa8bde6825625fe239a766ce58f9
pgk95 <kalupgk@gmail.com>
2023-11-03 19:31:01 +0300

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -9,4 +9,27 @@ babel = Babel(app)
 
 
 class Config(object):
-    
+    """config class for babel"""
+
+    DEBUG = True
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'  
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route("/")
+def index() -> str:
+    """index page"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
